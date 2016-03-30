e96982ad531f6de26361d13a226e1e56b5319724
Frank Santaguida <fjs138@gmail.com>
2016-03-30 15:30:52 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -12,7 +12,9 @@ sudo apt-get autoremove &&
 # is used to remove packages that were automatically installed to satisfy dependencies that are no longer needed
 sudo apt-get remove --purge $(deborphan) &&
 # finds packages that have no packages depending on them, that you did not explicitly install; removes them
-sudo apt-get -f install &&
+# sudo apt-get -f install &&
+# sudo apt-get install -f &&
+# this does not work in-script, but works from prompt, not sure why yet
 # attempt to correct a system with broken dependencies in place.
 sudo dpkg --configure -a
 # completes interrupted package operations
